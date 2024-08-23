1925ddda47df6c676989f8eb547fd14656804891
LicethRodriguez <licethrodriguez2005@gmail.com>
2024-08-23 16:21:55 -0500

    tipos_variables

---
+++ hola_mundo.py
@@ -0,0 +1,9 @@
+# Imprimir en pantalla hola mundo
+print ("¡Hola Mundo!")
+
+'''
+Nombre: Liceth Fernanda Rodriguez Pulido
+Materia: Programacion 1
+Correo: Licethrodriguez124@unisangil.edu.co
+
+'''
---
+++ tipos_variables.py
@@ -0,0 +1,10 @@
+#tipos de variables
+
+cadena = "Githut"
+print(type(cadena))
+numero = 10
+print(type(numero))
+decimal =  11.190
+print(type(decimal))
+boolean = True 
+print(type(boolean))
