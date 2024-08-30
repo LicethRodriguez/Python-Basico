f0e8a4768774c0ce7ace65adb3ac7b1b66b4df87
LicethRodriguez <licethrodriguez2005@gmail.com>
2024-08-30 15:18:13 -0500

    operadores.py

---
+++ transformacion_datos.py
@@ -0,0 +1,8 @@
+#capturar informacion
+
+nombre = input("Digite su nombre completo:")
+cedula= input("Digite su cedula: ")
+estatura = input("Digite su estatura:")
+estado = bool(input("Digite su estado:"))
+
+print(f"Su nombre es: {nombre}, su numero de cedula es: {cedula}, su estatura es: {estatura} y su estado es{estado} ")
