0dfe5b72cbd5029656c225a36de316659f761796
LicethRodriguez <licethrodriguez2005@gmail.com>
2024-08-30 15:34:05 -0500

    operadores.py

---
+++ operadores.py
@@ -0,0 +1,11 @@
+#importar libreria 
+import math
+#Ejemplo 1
+Ejemplo1 = 5*(2+5)
+#imprimir variable
+print(f"El resultado de la operacion es: {Ejemplo1}")
+
+#Ejemplo 2
+Ejemplo2 = (4+3)-((3*2)+1)
+#imprimir variable
+print(f"El resultado de la operacion es: {Ejemplo2}")
